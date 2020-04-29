7091160abe6013302f991b83af1ca75e8f7868c8
towasme <towasm@gmail.com>
2020-04-29 16:14:21 -0500

    changes

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ initializes the app """
-
 from flask import Blueprint
 app_views = Blueprint('app_views', __name__)
 
 
 from api.v1.views.index import *
+from api.v1.views.states import *
